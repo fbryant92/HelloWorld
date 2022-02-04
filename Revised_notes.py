a7943a9fd44d549863a77dbb19a2939e8d4ca424
fbryant92 <forrestcbryant@gmail.com>
2022-02-03 16:16:58 -0800

    first commit

---
+++ Revised_notes.py
@@ -0,0 +1,42 @@
+
+// List current directory
+ls -al
+
+// Create an new directory/folder
+mkdir <dir-name>
+
+// Change directory/folder
+cd <dir-name>
+
+// Change directory into the user root
+cd ~
+
+// Print out working directory
+pwd
+
+// Hard delete a folder or file
+rm -rf <folder-or-filename>
+
+// Initialize an empty local git repository
+git init 
+
+// Make a connection between local and remote repository
+git remote add origin <github-url>
+
+// Output the status of your working directory
+git status
+
+// Output a list of local branches
+git branch
+
+Rename File/Folder
+mv (old name) (New name)
+
+Create a new File
+touch (filename)
+
+Make New branch
+git checkout -b (name of branch)
+
+Show branch that youre on
+git branch
